767c69d53e8de85dac2907c0430b87e24d004d3b
contour66 <zamir.thind@gmail.com>
2021-06-19 18:39:02 -0700

    index.js

--- basic-calc/src/index.js
+++ basic-calc/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Calculator />
+    <Calculator/>
   </React.StrictMode>,
   document.getElementById('root')
 );
